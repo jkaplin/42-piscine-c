ce749973c61f7d00191a0d02177b971a49130836
jkaplin <kaplinjoseph@gmail.com>
2018-12-01 00:46:42 -0800

    day02 - ex04

--- days/day02/ex00/main.c
+++
@@ -1,7 +0,0 @@
-int	ft_print_alpha(void);
-
-int	main(void)
-{
-	ft_print_alpha();
-	return (0);
-}
---
+++ days/day02/ex04/ft_ptint_comb.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	digit[3];
+
+	digit[0] = '0' - 1;
+	digit[1] = '1' - 1;
+	digit[2] = '2' - 1;
+	while (digit[0] < digit[1] && digit[0] <= '7')
+	{
+		digit[1] = ++digit[0] + 1;
+		while ((digit[1] < digit[2] && digit[1] <= '8') || (digit[2] == '1'))
+		{
+			digit[2] = digit[1] + 1;
+			while (digit[2] <= '9')
+			{
+				ft_putchar(digit[0]);
+				ft_putchar(digit[1]);
+				ft_putchar(digit[2]++);
+				if (digit[0] != '7')
+				{
+					write(1, ",", 1);
+					write(1, " ", 1);
+				}
+			}
+			digit[1]++;
+		}
+	}
+}
